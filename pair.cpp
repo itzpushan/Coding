ea3a6eb987d782016bdb0700bc95799856050e0a
itzpushan <128570040+itzpushan@users.noreply.github.com>
2023-06-29 16:11:36 +0530

    Add files via upload

---
+++ pair.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+signed main() {
+std::ios::sync_with_stdio(false);
+std::cin.tie(nullptr); std::cout.tie(nullptr);
+
+pair<int , int> p= {1,3};
+
+cout << p.first << "\t" << p.second <<endl;
+
+pair<int , int> arr[]={ {1,2}, {1,7}, {2,23}};
+cout << arr[1].second;
+
+return 0;
+}
---
+++ vector.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+signed main() {
+std::ios::sync_with_stdio(false);
+std::cin.tie(nullptr); std::cout.tie(nullptr);
+
+// <<<<<<<<<<<<-------------------VECTOR is dynamicArray inC++--------------------->>>>>>>>>>>>>    
+
+/*  vector <int> v;
+    v.push_back(1);
+    v.emplace_back(2);
+    //<<<<<<<<<<----------emplace_back is faster than push_back------------->>>>>>>>>>>>>>>>>
+
+    cout << v[0] << " " << v[1] << endl;
+*/
+
+/*
+    //<<<<<<<<---------contains five instances of 100------->>>>>>>
+    vector <int> v(5,100);
+    cout << v[0] << " " << v[1] << " " << v[2] << " " << v[3] << " " << v[4] << endl;
+*/
+
+/*
+    //<<<<<<<<<<<--------Stores 0-------->>>>>>>>>>
+   vector <int> v(5);
+   cout << v[0] << " " << v[1] << " " << v[2] << " " << v[3] << " " << v[4] << endl;
+*/
+/*
+    vector <int> v1(5,20);
+    vector <int> v(v1);
+    cout << v[0] << " " << v[1] << " " << v[2] << " " << v[3] << " " << v[4] << endl;
+*/
+
+return 0;
+}

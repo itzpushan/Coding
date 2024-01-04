32005fe2b6e19d9f35d912cef00a6db113e39069
itzpushan <pushank@iitbhilai.ac.in>
2024-01-04 22:01:01 +0530

    preorder and postorder traversal

---
+++ tree/binary_tree/3_preorder_traversal.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+        int data;
+        Node* left;
+        Node* right;
+
+        Node(int data){
+            this->data = data;
+            this->left = NULL;
+            this->right = NULL;
+        }
+};
+
+void inorderTraversal(Node* root){
+    if(root==NULL)
+        return;
+    cout << root->data <<" ";    
+    inorderTraversal(root->left);
+    inorderTraversal(root->right);   
+}
+
+int main(){
+
+    class Node* root = new Node(17);
+    root->left= new Node(3);
+    root->right= new Node(7);
+    root->left->left = new Node(43);
+    root->left->right = new Node(38);
+    root->right->right = new Node(82);
+
+    cout << "The preorder traversal results in:" << endl;
+    inorderTraversal(root);
+    cout << endl;
+
+    return 0;
+}
